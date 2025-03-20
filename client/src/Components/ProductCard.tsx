8c9ac87d0a423e95e0f0a48c3fe94e5baba652b8
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2025-03-20 22:15:04 +0530

    removed error

--- client/src/Components/ProductCard.tsx
+++ client/src/Components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import useReverseGeocoding from "../Hooks/useReverseGeocoding";
 
 interface Product {
--- client/src/Components/UpdateProfile.tsx
+++ client/src/Components/UpdateProfile.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import * as React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useQuery, useMutation } from "@tanstack/react-query";
